bdf23fb5de8f090321522ae6c2eb691962aadb82
pdbro2k <40125493+pdbro2k@users.noreply.github.com>
2021-12-23 11:46:15 +0100

    fixed string escaping

--- resources/js/buildCrossTable.js
+++ resources/js/buildCrossTable.js
@@ -3,14 +3,18 @@
 function buildTable(nodes, tableId) {
 	
 	// local serialization helper functions
+	function escape(text) {
+		return text.toString().replaceAll("'", "&apos;").replaceAll('"', "&quot;");
+	}
+	
 	function buildTHCell(textData, id) {
-		return $("<th title='" + textData[id] + "'>" + id + "</th>");
+		return $("<th title='" + escape(textData[id]) + "'>" + id + "</th>");
 	}
 	function buildTDCell(currentBigrams, comparableBigrams) {
 		let commonBigrams = getCommon(currentBigrams, comparableBigrams);
 		let currentValue = commonBigrams.length / currentBigrams.length;
 		let currentPercentage = parseFloat( 100 * currentValue ).toFixed(1) + "%";
-		return $("<td title='" + commonBigrams + "'>" + currentPercentage + "</td>");
+		return $("<td title='" + escape(commonBigrams) + "'>" + currentPercentage + "</td>");
 	}
 	
 	// main function
